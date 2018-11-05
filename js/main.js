82b78416b90a0c2c56737e49e458de38f804b7ac
Laura Domingo <laura.domingo.82@gmail.com>
2018-11-05 04:00:46 -0800

    funcionalidad seleccionado y deseleccionados todos

--- js/main.js
+++ js/main.js
@@ -3,6 +3,10 @@
 const url ='https://raw.githubusercontent.com/Adalab/recipes-data/master/rissoto-setas.json';
 let result;
 const list = document.querySelector('.ingredients-list');
+let rowList= new Array();
+const titel= document.querySelector('.titel');
+const total= document.querySelector('.total');
+const shippingCost= document.querySelector('.shipping-cost');
 
 fetch(url)
     .then(res => res.json())
@@ -15,28 +19,70 @@ fetch(url)
 function listProducts(){
     const product= result.recipe.ingredients;
     console.log(product);
-    const titel= document.querySelector('.titel');
-        titel.innerHTML=result.recipe.name;
+
+    titel.innerHTML=result.recipe.name;
 
     for (var i = 0; i < product.length; i++) {
-      var listItem = document.createElement('li');
-      listItem.classList.add('items-li');
+      rowList[i] = document.createElement('li');
+      rowList[i] .classList.add('items-li');
       const check = document.createElement('input');
-      const quantityNumber = document.createElement ('input');
+      check.classList.add('select-item');
+      const number = document.createElement ('input');
       const article = document.createElement('h2');
       const brand = document.createElement('p');
       const quantity = document.createElement('p');
       const price = document.createElement('p');
+      price.classList.add('price-item');
 
+      check.type='checkbox';
+      check.name = 'products';
+      number.type= 'number';
       article.innerHTML = product[i].product;
       brand.innerHTML = product[i].brand;
       quantity.innerHTML =  product[i].quantity;
       price.innerHTML = product[i].price + ' € '
 
-      list.appendChild(listItem);
-      listItem.appendChild(article);
-      listItem.appendChild(brand);
-      listItem.appendChild(quantity);
-      listItem.appendChild(price);
+      list.appendChild(rowList[i] );
+      rowList[i].appendChild(check);
+      rowList[i].appendChild(number);
+      rowList[i].appendChild(article);
+      rowList[i].appendChild(brand);
+      rowList[i].appendChild(quantity);
+      rowList[i].appendChild(price);
+
+      shippingCost.innerHTML="Gastos de evio: " + result.recipe['shipping-cost'] + ' €'
+
+      check.addEventListener('click', selectOne);
+
     }
 }
+
+function selectOne(event){
+console.log('kk');
+console.log(rowList);
+// for (let i=0; i<rowList.length;i++)
+// total.innerHTML= rowList[i].price;
+}
+
+
+function selectAll(){
+  const elements= document.form.elements;
+    for (let i=0; i<elements.length;i++)
+       if(elements[i].type == "checkbox")
+          elements[i].checked=1
+ }
+
+ function deselectAll(){
+   const elements= document.form.elements;
+    for (let i=0; i<elements.length;i++) {
+       if(elements[i].type == "checkbox")
+          elements[i].checked=0;
+    }
+ }
+
+ const select = document.querySelector('.select-all');
+ const deselect = document.querySelector('.deselect-all');
+
+
+ select.addEventListener('click', selectAll);
+ deselect.addEventListener('click', deselectAll);
